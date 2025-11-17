693d5027d1bdb65de8ea5e736a494ceaac58dbf5
Faldi <faldiansyahk@gmail.com>
2025-11-17 07:20:47 +0600

    game area

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,30 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                'Game area (coming soon)',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text('Play', style: TextStyle(color: Colors.white)),
+                Text('Exit', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
